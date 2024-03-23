5f5f4ec8f356c4b4b87fb350b25042c6ff111501
atk-titan <gauravmane8451@gmail.com>
2024-03-23 19:21:31 +0530

    login and exception

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-setup(
-#     name="First_project",
-#     version='0.0.1',
-#     author='Gaurav',
-#     author_email='gauravmane8451@gmail.com',
-#     packages=find_packages(),
-#     install_requires=read_requirements('requirements.txt') ,  # Use install_requires for dependencies
-# )
