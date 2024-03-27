1df79666dfcdffa3b92c9fa8cb4371c5ef17d638
atk-titan <gauravmane8451@gmail.com>
2024-03-28 00:59:57 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 from dataclasses import dataclass
 
@@ -48,7 +49,12 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__ == '__main__':
-    train_data,test_data=DataIngestion().initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation().initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 # Assuming CustomException class is defined in src.exception module
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,15 +1,17 @@
-import sys 
-import os
-from sklearn.preprocessing import *
+import sys
+from dataclasses import dataclass
+
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from dataclasses import dataclass 
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+import os
+
 from src.utils import save_object
 
 @dataclass
@@ -19,8 +21,12 @@ class DataTransformationConfig:
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
-    
+
     def get_data_transformer_object(self):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
